9dbda515ba81c830a559debf2a8f83f432e4db68
Betarost <betarost@yandex.ru>
2023-06-16 14:39:05 +0300

    Init First

---
+++ index.d.ts
@@ -0,0 +1,33 @@
+interface Static {
+    [elemName: string]: any;
+}
+
+interface CEM {
+    [elemName: string]: any;
+}
+
+interface Map {
+    [elemName: string]: any;
+}
+
+interface jsxResult {
+    tag: String,
+    data: any,
+    children: any[]
+}
+
+interface Micro {
+    name: String,
+    loader: Function,
+    display: Function,
+    Static?: Static
+}
+
+
+export declare function Cemjsx(tag: String, data: any, ...children: any[]): jsxResult
+
+export declare function load(micro: Micro): void
+
+export declare function initMap(micro: Map): void
+
+export declare let CEM: CEM
